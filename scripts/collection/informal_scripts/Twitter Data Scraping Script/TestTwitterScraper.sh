bc81cae75aed11ea6104b0f329cdcca9d11b1fd9
Quinton Jasper <qjj2@nau.edu>
2022-06-14 20:23:39 -0700

    Progress push for wednesday

--- scripts/collection/informal_scripts/Twitter Data Scraping Script/TestTwitterScraper.sh
+++ scripts/collection/informal_scripts/Twitter Data Scraping Script/TestTwitterScraper.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
 # Constant Values
-START_DATE="2017-01-01"
+START_DATE="2017-01-01" # date format: YYYY-mm-dd
 DATE_NOW=$(date +%Y-%m-%d)
 KEYWORDS_FILE=$1
 TWEET_LIMIT=10
 
 TWARC_CONFIG="$HOME/.config/twarc/config"
 
-gen_file_name() {
+function gen_file_name {
     local START=$1
     local END=$2
 
     printf "%s_%s_twitter_data.csv" "$START" "$END"
 }
 
-inc_date_by_X_year(){
+function help_output {
+    printf "\nTwitter Scraper - Scrapes tweets from $START_DATE to Now. Start date can be changed.\n\nUsage:\n\ntwitterscraper.sh [ --help | -h ] {KEYWORD_FILE_DIR}"
+
+}
+
+function inc_date_by_X_year {
     local DATE_STR=$1
     local NUM_YEARS=$2
 
@@ -23,7 +28,7 @@ inc_date_by_X_year(){
     echo "$NEW_DATE"
 }
 
-gen_one_yr_inc(){
+function gen_one_yr_inc {
     local DATE_STR=$(date +%Y-%m-%d -d "$1")
     local INC=$(inc_date_by_X_year "$DATE_STR" 1)
     
@@ -39,7 +44,12 @@ gen_one_yr_inc(){
 
 main() {
 
-    echo "Current Dir: $PWD"
+    if [ "$KEYWORDS_FILE" = "--help" ] || [ "$KEYWORDS_FILE" = "-h" ]; then
+        help_output
+        exit 1
+    fi
+
+
 
     if ! command -v twarc2 &>/dev/null; then
 
@@ -49,16 +59,28 @@ main() {
     fi
 
     if ! [[ -f "$TWARC_CONFIG" ]]; then
-        echo "Don't forget to configure twitter keys/secrets with twarc"
+        echo "Don't forget to configure twitter keys/secrets with **twarc2**"
         echo "https://twarc-project.readthedocs.io/en/latest/twarc2_en_us/#configure"
         exit 1
     fi
 
-    gen_one_yr_inc 2022-01-01
 
-    # use twarc to grab historical tweets from 01/2017 to now
-    #twarc2 searches --archive --counts-only --granularity day --limit "$TWEET_LIMIT" --start-time "$START_DATE" --end-time "$END_DATE" "$KEYWORDS_FILE" "$(gen_file_name $START_DATE $DATE_NOW)"
 
+    # Now we can get into the program iteration
+    local END_DATE=$(gen_one_yr_inc $START_DATE)
+
+    while [ "$(date +%s -d "$END_DATE")" -lt "$(date +%s -d "$DATE_NOW")" ]; do
+
+        # use twarc to grab historical tweets from 01/2017 to now
+        # TODO - Determine optimal settings for twarc
+        echo "twarc2 searches --archive --counts-only --granularity day --limit $TWEET_LIMIT --start-time $START_DATE --end-time $END_DATE $KEYWORDS_FILE $(gen_file_name $START_DATE $END_DATE)"
+        twarc2 searches --combine-queries --archive --granularity day --limit "$TWEET_LIMIT" --start-time "$START_DATE" --end-time "$END_DATE" "$KEYWORDS_FILE" "$(gen_file_name $START_DATE $END_DATE)"
+        START_DATE=$(gen_one_yr_inc $START_DATE)
+        END_DATE=$(gen_one_yr_inc $END_DATE)
+
+    done
+
+    exit 0
 }
 
 
---
+++ scripts/collection/informal_scripts/Twitter Data Scraping Script/project_progression.md
@@ -0,0 +1,11 @@
+# Project progression plan - Twitter Scraper
+
+## Step-by-step
+
+1. Gather all tweets that are within the age-range of "2017-01-01" up to now
+
+2. Implement cron job to automatically ping for more tweets after X seconds/minutes/hours
+
+3. Convert the generated JSON file to a properly formatted CSV (tab-separated)
+
+
